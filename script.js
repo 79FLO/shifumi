3da9aaa85ab06a896fa17cb5df476de73cec7033
Steff <steff.1@gmail.com>
2022-11-16 16:51:52 +0100

    fonction shi avec bouton

--- script.js
+++ script.js
@@ -4,9 +4,32 @@ const mi = document.getElementById('mi');
 const player = document.getElementById('player');
 const ia = document.getElementById('ia');
 const command = document.getElementById('command');
+let choixOrdi=["Shi","Fu","Mi"];
 
-shi.addEventListener('click', () => console.log('shi'));
+function resultRandom () {
+    return choixOrdi[Math.floor(Math.random() * choixOrdi.length)]
+}
+let scoreIa = 0;
+let scorePlayer = 0;
+
+shi.addEventListener('click', function() {
+        if (resultRandom()=="Shi"){
+            console.log(resultRandom());
+            console.log("Egal!"); 
+        }
+        else if (resultRandom()=="Fu"){
+            document.getElementById('score-player').innerHTML = ++scorePlayer;
+            console.log(resultRandom());
+            console.log("You Win!");
+        }
+        else {
+            document.getElementById('score-ia').innerHTML = ++scoreIa;;
+            console.log(resultRandom());
+            console.log("You Loose!");
+        }
+    });
 fu.addEventListener('click', () => console.log('fu'));
 mi.addEventListener('click', () => console.log('mi'));
 
-// Pour le reste, a vous de jouer
+
+// Pour le reste, a vous de jouer */
